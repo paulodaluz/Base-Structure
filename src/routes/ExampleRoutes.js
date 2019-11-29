56b6b8eefc1900d2fb6046582c56daa658533e3e
Paulo da Luz <paulo.luz@compasso.com.br>
2019-11-29 17:30:34 -0300

    Criando rota de exemplo

--- src/routes/ExampleRoutes.js
+++ src/routes/ExampleRoutes.js
@@ -3,7 +3,7 @@ var express = require('express');
 const router = express();
 
 router.route('/exampleRoute')
-    .get(function (req, res) { res.send('Get a random book') })
-    .post(function (req, res) { res.send('Add a book') })
+    .get(function (req, res) { res.send('Example Get') })
+    .post(function (req, res) { res.send('Example Post') })
 
 module.exports = router;
